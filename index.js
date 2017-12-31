4a5f2744c2f1f1861159d14ccf33040ba288cb10
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-31 00:17:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var customerName = 'bob';
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
-var bestCustomer = undefined;
 
 function setBestCustomer() {
  var bestCustomer = 'not Bob';
